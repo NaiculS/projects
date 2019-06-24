0f1d94a6eb2b162a3cf2982d68b7e76cf7701dad
NaiculS <30321702+NaiculS@users.noreply.github.com>
2019-06-24 16:23:47 -0400

    Add files via upload

---
+++ learning/idtech/PrintingStuff/ArithmeticExample.java
@@ -0,0 +1,22 @@
+public class ArithmeticExample {
+    public static void Arithmetic(){
+        int Number1=2;
+        int Number2=100;
+        Number1+=10;
+        Number1--;
+        Number2*=Number1;
+        Number2++;
+        System.out.println("Number 1: " + Number1);
+        System.out.println("Number 2: " + Number2);
+    }
+    public static void main(String[] args) {
+        int Number1=2;
+        int Number2=100;
+        Number1+=10;
+        Number1--;
+        Number2*=Number1;
+        Number2++;
+        System.out.println("Number 1: " + Number1);
+        System.out.println("Number 2: " + Number2);
+    }
+}
