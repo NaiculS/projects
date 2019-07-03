572f8f51b706a459f9288bb4c91c1fd36e8095b5
NaiculS <30321702+NaiculS@users.noreply.github.com>
2019-07-03 16:47:36 -0400

    Delete Array.java

--- learning/idtech/PrintingStuff/Array.java
+++
@@ -1,6 +0,0 @@
-public class Array {
-    public static void main(String[] args) {
-        String[]Array={"Kaiba","Lucian","Bella","Andrew","Sebastian"};
-        System.out.println(Array[3] + " is absent today.");
-    }
-}
